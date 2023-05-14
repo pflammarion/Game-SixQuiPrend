3b3689d5ab0dbbbef0da4428a27ec11a0a2b1b5c
Lilla Bulte <lilla.bulte@gmail.com>
2023-05-15 00:31:12 +0200

    add font ui elements

--- src/main/java/org/isep/sixquiprend/view/GUI/scenes/WelcomeView.java
+++ src/main/java/org/isep/sixquiprend/view/GUI/scenes/WelcomeView.java
@@ -1,27 +1,74 @@
 package org.isep.sixquiprend.view.GUI.scenes;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import org.isep.sixquiprend.model.Player;
+
+import java.util.List;
 
 public class WelcomeView {
-
-    // Déclarer les éléments ici
+    
     private final Button buttonQuit;
+    private final Button buttonAjouter;
+    private final Button buttonPlay;
+    private final TextField playerNameTextField;
+    private final ListView<String> playerList;
     private final Scene scene;
     public WelcomeView() {
 
         //ImageView imageView = new ImageView(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/isep/harrypotter/assets/img/background/home-background.jpg"))));
-
-
-        Label helloLabel = new Label("Hello");
-
         this.buttonQuit = new Button("Quit");
 
-        VBox vbox = new VBox(helloLabel, buttonQuit);
+        Label playerNameLabel = new Label("Nom du joueur");
+        Label playerListLabel = new Label("Joueurs");
+        Label gameName = new Label("SUPER six qui prend");
+
+        this.playerNameTextField = new TextField();
+        this.playerNameTextField.setMaxWidth(200);
+        this.playerNameTextField.setPromptText("Entrer le nom");
+
+        VBox playerNameVBox = new VBox(playerNameLabel, playerNameTextField);
+        playerNameVBox.setSpacing(20);
+        playerNameVBox.setAlignment(Pos.CENTER_LEFT);
+
+        this.buttonAjouter = new Button("Ajouter");
+        this.buttonAjouter.setAlignment(Pos.CENTER);
+        this.buttonAjouter.setPrefSize(200, 50);
+
+        HBox playerAddHBox = new HBox(playerNameVBox, buttonAjouter);
+        playerAddHBox.setSpacing(20);
+        playerAddHBox.setAlignment(Pos.BASELINE_CENTER);
+
+
+        this.buttonPlay = new Button("Jouer !");
+        this.buttonPlay.setAlignment(Pos.CENTER);
+        this.buttonPlay.setPrefSize(400, 100);
+        buttonPlay.getStyleClass().add("play_button");
+
+        VBox playerSetVBox = new VBox(playerAddHBox, buttonPlay);
+        playerSetVBox.setAlignment(Pos.CENTER);
+
+       this.playerList = new ListView<>();
+       this.playerList.setMaxSize(200, 200);
+
+        VBox playerListVBox = new VBox(playerListLabel, playerList);
+        playerListVBox.setAlignment(Pos.CENTER);
+
+        HBox playerContainerHBox = new HBox(playerListVBox, playerSetVBox);
+        playerContainerHBox.setSpacing(50);
+        playerContainerHBox.setAlignment(Pos.CENTER);
+
+
+        VBox vbox = new VBox(gameName, playerContainerHBox);
         vbox.setSpacing(10);
         vbox.setAlignment(Pos.CENTER);
 
@@ -30,6 +77,7 @@ public class WelcomeView {
 
         AnchorPane anchorPane = new AnchorPane(vbox);
         anchorPane.setPrefSize(1200, 600);
+        AnchorPane.setTopAnchor(vbox, 100.0);
         AnchorPane.setBottomAnchor(vbox, 100.0);
         AnchorPane.setLeftAnchor(vbox, 300.0);
         AnchorPane.setRightAnchor(vbox, 300.0);
@@ -46,5 +94,23 @@ public class WelcomeView {
 
     public Button getButtonQuit() {
         return buttonQuit;
+    }
+
+    public Button getButtonAjouter() {
+        return buttonAjouter;
+    }
+
+    public Button getButtonPlay() {
+        return buttonPlay;
+    }
+
+    public void setPlayerList(List<Player> list) {
+        List<String> strings = list.stream()
+                .map(Player::toString)
+                .toList();
+        ObservableList<String> observableList = FXCollections.observableArrayList(strings);
+        this.playerList.setItems(observableList);
+        this.playerList.setSelectionModel(null);
+        this.playerList.getStyleClass().add("player-list");
     }
 }
