f9d14f7c558f147f8f1719fac83afbc681fa4537
Sh0lf <vyvincentyap@gmail.com>
2023-05-14 22:21:12 +0200

    Done with supposed setup

--- src/main/java/org/isep/sixquiprend/view/GUI/SceneManager.java
+++ src/main/java/org/isep/sixquiprend/view/GUI/SceneManager.java
@@ -3,20 +3,18 @@ package org.isep.sixquiprend.view.GUI;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SceneManager {
+
     private final Stage stage;
     private final Map<String, Scene> scenes;
-    private final List<String> viewQueue;
-    private int currentViewIndex;
+    private String currentSceneName;
 
     public SceneManager(Stage stage) {
         this.stage = stage;
         this.scenes = new HashMap<>();
-        this.viewQueue = new ArrayList<>();
-        this.currentViewIndex = -1;
-        stage.setTitle("Six qui prend");
     }
 
     public void addScene(String sceneName, Scene scene) {
@@ -30,39 +28,12 @@ public class SceneManager {
             throw new IllegalArgumentException("Scene not found: " + sceneName);
         }
 
-        viewQueue.remove(sceneName);
-        currentViewIndex = viewQueue.indexOf(sceneName);
-        //scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/com/isep/sixquiprend/assets/css/style.css")).toExternalForm());
+        currentSceneName = sceneName;
         stage.setScene(scene);
     }
 
-    public void addViewToQueue(String sceneName) {
-        viewQueue.add(sceneName);
-    }
-
-    public void switchToNextScene() {
-        if (viewQueue.isEmpty()) {
-            throw new IllegalStateException("View queue is empty");
-        }
-
-        currentViewIndex++;
-        if (currentViewIndex >= viewQueue.size()) {
-            currentViewIndex = 0;
-        }
-        String currentView = viewQueue.get(currentViewIndex);
-        viewQueue.remove(currentView);
-        switchToScene(currentView);
-    }
-
     public String getCurrentSceneName() {
-        return viewQueue.get(currentViewIndex);
+        return currentSceneName;
     }
 
-    public String getNextSceneName() {
-        int nextIndex = currentViewIndex + 1;
-        if (nextIndex >= viewQueue.size()) {
-            nextIndex = 0;
-        }
-        return viewQueue.get(nextIndex);
-    }
 }
