2b4277b3e33059ba96db2d367c04fd783793729d
Sh0lf <vyvincentyap@gmail.com>
2023-05-14 22:55:20 +0200

    Partially done

--- src/main/java/org/isep/sixquiprend/model/Card.java
+++ src/main/java/org/isep/sixquiprend/model/Card.java
@@ -1,7 +1,13 @@
 package org.isep.sixquiprend.model;
 
-public class Card {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
+@AllArgsConstructor
+public class Card {
     private int number;
     private int value;
     private String name;
