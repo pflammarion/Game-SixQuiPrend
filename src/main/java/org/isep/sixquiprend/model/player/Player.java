5847640e9a3412be1be2e0a4aef7e3226ea5300a
Sh0lf <vyvincentyap@gmail.com>
2023-05-16 12:08:42 +0200

    Added purpose of variable cardPlayed, comments

--- src/main/java/org/isep/sixquiprend/model/player/Player.java
+++ src/main/java/org/isep/sixquiprend/model/player/Player.java
@@ -10,4 +10,11 @@ import java.util.List;
 public abstract class Player {
     private List<Card> hand = new ArrayList<>();
     private int score = 0;
+    private Card cardPlayed;
+    // cardPlayed utilisé pour référer le joueur et la carte jouée de la main pour le board.
+    // Exemple 1: si 6eme carte jouée sur une row, il faut savoir qui a joué la 6eme carte, pour pouvoir
+    // donner toutes les cartes du row.
+
+    // Exemple 2: si la carte jouée par un joueur ne peut pas être poser sur une des rows, le joueur doit récupérer
+    // toutes les cartes d'une row pour pouvoir placer sa carte
 }
