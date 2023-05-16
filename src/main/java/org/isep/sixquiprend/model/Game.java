bb49fbb8781d05fb7f17c1720f6cc01313644a7f
Paul Flammarion <paul@flammarion.eu>
2023-05-16 23:16:28 +0200

    add game methode

--- src/main/java/org/isep/sixquiprend/model/Game.java
+++ src/main/java/org/isep/sixquiprend/model/Game.java
@@ -17,4 +17,23 @@ public class Game {
     private int round = 1;
     private int totalBullHeads = 0;
     private boolean gameEnded = false;
+    private int currentPlayerIndex = 0;
+
+    public void moveToNextPlayer() {
+        currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
+    }
+    public Player getCurrentPlayer(){
+        return players.get(currentPlayerIndex);
+    }
+    public void incrementRound() {
+        this.round += 1;
+    }
+    public void reset() {
+        players.clear();
+        cardsPlayed.clear();
+        board.clear();
+        round = 1;
+        totalBullHeads = 0;
+        gameEnded = false;
+    }
 }
