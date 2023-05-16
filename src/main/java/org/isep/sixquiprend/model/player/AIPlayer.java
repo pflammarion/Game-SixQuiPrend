e1a1c0b74b87d9bce81c2b81218c9233c7b7f532
Paul Flammarion <paul@flammarion.eu>
2023-05-16 23:50:12 +0200

    update logic

--- src/main/java/org/isep/sixquiprend/model/player/AIPlayer.java
+++ src/main/java/org/isep/sixquiprend/model/player/AIPlayer.java
@@ -1,4 +1,11 @@
 package org.isep.sixquiprend.model.player;
 
+import org.isep.sixquiprend.model.Card;
+
+import java.util.List;
+
 public class AIPlayer extends Player {
+    public AIPlayer(String name) {
+        super(name);
+    }
 }
