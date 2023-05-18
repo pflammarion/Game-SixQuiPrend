d4139aec0ae5585f2909d6f8dc7481067d691ffa
Sh0lf <vyvincentyap@gmail.com>
2023-05-18 03:01:57 +0200

    Reorganize methods from Game to GameController + added the 2 main rules of smallest diff row selected and if impossible, take the row with the less points/bullheads. CAN BE USED FOR AI

--- src/main/java/org/isep/sixquiprend/model/Game.java
+++ src/main/java/org/isep/sixquiprend/model/Game.java
@@ -24,40 +24,4 @@ public class Game {
             this.board.add(new ArrayList<>());
         }
     }
-
-
-    public void moveToNextPlayer() {
-        currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
-    }
-    public Player getCurrentPlayer(){
-        return players.get(currentPlayerIndex);
-    }
-    public void incrementRound() {
-        this.round += 1;
-    }
-    public void reset() {
-        players.clear();
-        cardsPlayed.clear();
-        board.clear();
-        boardSetUp();
-        round = 1;
-        totalBullHeads = 0;
-        gameEnded = false;
-    }
-    public int updateBoard(List<Object> played) {
-        int score = 0;
-        List<Card> selectedRow = this.board.get((Integer) played.get(1));
-
-        if (selectedRow.size() < 6) {
-            selectedRow.add((Card) played.get(0));
-        } else {
-            for (Card card : selectedRow) {
-                score += card.getBullHeads();
-            }
-            selectedRow.clear();
-        }
-        board.set((Integer) played.get(1), selectedRow);
-
-        return score;
-    }
 }
