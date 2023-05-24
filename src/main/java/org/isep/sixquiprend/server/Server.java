980495f17698f3526df657bb3888fa4feff30ee9
Paul Flammarion <paul@flammarion.eu>
2023-05-24 23:43:36 +0200

    update script

--- src/main/java/org/isep/sixquiprend/server/Server.java
+++ src/main/java/org/isep/sixquiprend/server/Server.java
@@ -1,18 +1,15 @@
 package org.isep.sixquiprend.server;
 
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 
     public static void main(String[] args) {
-        int port = 4444; // Le port sur lequel le serveur écoutera les connexions
+        int port = 4444;
 
         try {
-            // Créer une instance de ServerSocket pour écouter les connexions entrantes
             ServerSocket serverSocket = new ServerSocket(port);
             System.out.println("Le serveur est en attente de connexions sur le port " + port);
 
@@ -20,7 +17,6 @@ public class Server {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Nouvelle connexion entrante : " + clientSocket);
 
-                // Créer un thread pour gérer la communication avec le client
                 ClientHandler clientHandler = new ClientHandler(clientSocket);
                 clientHandler.run();
             }
