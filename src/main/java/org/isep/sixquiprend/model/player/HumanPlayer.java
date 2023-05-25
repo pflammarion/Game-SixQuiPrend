3792f659dd38887fca389a2f80d920c48362f8ee
Paul Flammarion <paul@flammarion.eu>
2023-05-25 18:32:01 +0200

    update to serializable

--- src/main/java/org/isep/sixquiprend/model/player/HumanPlayer.java
+++ src/main/java/org/isep/sixquiprend/model/player/HumanPlayer.java
@@ -5,11 +5,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.isep.sixquiprend.model.Card;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Getter
 @Setter
-public class HumanPlayer extends Player {
+public class HumanPlayer extends Player implements Serializable {
     public HumanPlayer(String name) {
         super(name);
     }
