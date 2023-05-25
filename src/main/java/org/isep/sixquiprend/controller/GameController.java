5735304f4c4a8ed2a0271b000f6a75dcb5eaf993
Paul Flammarion <paul@flammarion.eu>
2023-05-25 21:21:39 +0200

    remove system out

--- src/main/java/org/isep/sixquiprend/controller/GameController.java
+++ src/main/java/org/isep/sixquiprend/controller/GameController.java
@@ -137,7 +137,6 @@ public class GameController {
                     .collect(Collectors.toList());
             gameInfo.addAll(playerList);
 
-            System.out.println(gameInfo);
             client.sendMessageToServer(gameInfo);
 
         }
