c2c2d474954575814a045faf437ee8188a8c8523
Sh0lf <vyvincentyap@gmail.com>
2023-05-26 11:50:51 +0200

    Added music in Loop for all the scenes

--- src/main/java/org/isep/sixquiprend/view/GUI/scenes/WelcomeView.java
+++ src/main/java/org/isep/sixquiprend/view/GUI/scenes/WelcomeView.java
@@ -13,8 +13,13 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.util.Duration;
 import org.isep.sixquiprend.model.player.Player;
 
+import java.io.File;
 import java.util.List;
 import java.util.Objects;
 
@@ -105,6 +110,23 @@ public class WelcomeView {
 
         //imageView.fitWidthProperty().bind(scene.widthProperty());
         //imageView.fitHeightProperty().bind(scene.heightProperty());
+
+        //Initialising path of the media file, replace this with your file path
+        String path = "src/main/resources/org/isep/sixquiprend/assets/ThomasLeGoat.mp3";
+
+        //Instantiating Media class
+        Media media = new Media(new File(path).toURI().toString());
+
+        //Instantiating MediaPlayer class
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+
+        MediaView mediaView = new MediaView(mediaPlayer);
+
+        mediaPlayer.setOnEndOfMedia(() -> mediaPlayer.seek(Duration.ZERO));
+        mediaPlayer.play();
+
+        // Add the MediaView to the scene or any layout container
+        anchorPane.getChildren().add(mediaView);
     }
 
     public Scene getScene() {
