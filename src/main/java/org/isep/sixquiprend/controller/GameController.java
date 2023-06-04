dcb4311adc3a4104b1e8744bf5c230b132ecd8d9
Sh0lf <vyvincentyap@gmail.com>
2023-06-04 20:52:14 +0200

    Fixed to previous proposition, do the loadingView only when >=2 HumanPlayer

--- src/main/java/org/isep/sixquiprend/controller/GameController.java
+++ src/main/java/org/isep/sixquiprend/controller/GameController.java
@@ -164,11 +164,13 @@ public class GameController {
                     countHuman++;
                 }
             }
-            if (countHuman >= 2) {
+            if (countHuman < 2){
+                playCard();
+            } else {
                 loadingView.setConcernedPlayer(humanPlayer.getName());
+                loadingView.setRoundCounter("Round "+ game.getRound());
+                sceneManager.switchToScene("loading");
             }
-            loadingView.setRoundCounter("Round "+ game.getRound());
-            sceneManager.switchToScene("loading");
         }
     }
 
